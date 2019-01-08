3862f6563b5f538b29f5d74612165abc8f1094b6
A-Mac <albtran@ucdavis.edu>
2019-01-08 15:47:32 -0800

    Update generateRandomKey.js

--- generateRandomKey.js
+++ generateRandomKey.js
@@ -1,3 +1,5 @@
+//https://jonathas.com/token-based-authentication-in-nodejs-with-passport-jwt-and-bcrypt/
+// use above link to store token into a .env file
 // Generate a v4 UUID (random)
 const uuidV4 = require('uuid/v4');
 
